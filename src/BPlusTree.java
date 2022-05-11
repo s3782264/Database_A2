215cd98d6f637b51021511801b52bbfe2666bbeb
Angela Alviano <s3782264@student.rmit.edu.au>
2022-05-11 15:22:20 +1000

    Reading heapfile implemented

--- src/BPlusTree.java
+++ src/BPlusTree.java
@@ -1,11 +1,37 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class BPlusTree {
+	private BufferedReader reader;
+	private long startTime;
+	private long endTime;
 	public BPlusTree()
 	{
 		
 	}
 	
-	public void run() {
-		
+	public void run(int pageSize, String file) throws FileNotFoundException {
+		// Open the heap file for reading
+		reader = new BufferedReader(new FileReader(file));
+		// Set the start time
+		startTime = System.currentTimeMillis();
+		String line = "";
+		try {
+			// Iterate through the lines in the file
+			while ((line = reader.readLine()) != null) {
+				System.out.println(line);
+			}
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		// Set the end time
+		endTime = System.currentTimeMillis();
+		// Print info to the console
+		System.out.println("Records Loaded: " );
+		System.out.println("Page Count: " );
+		System.out.println("Time Taken (milliseconds): " + (endTime - startTime));
 	}
 }
--- src/btindex.java
+++ src/btindex.java
@@ -1,12 +1,13 @@
+import java.io.FileNotFoundException;
 
 public class btindex {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws NumberFormatException, FileNotFoundException {
 		// TODO Auto-generated method stub
 		if(args[0].equals("-p") && args.length ==3)
 		{
 			BPlusTree tree = new BPlusTree();
-			tree.run();
+			tree.run(Integer.parseInt(args[1]), args[2]);
 		} else {
 			System.out.println("Incorrect Arguments");
 		}
